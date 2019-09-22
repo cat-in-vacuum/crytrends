dc92b8ef39007af9d3f7a53b11f7b4283c9a09fb
DN200891UER2 <egor.uralov@privatbank.ua>
2019-09-22 21:05:05 +0300

    minor fixes

--- main.go
+++ main.go
@@ -1,23 +1,31 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"github.com/cat-in-vacuum/crytrade/providers/cryptowat"
 	"net/http"
+	"time"
 )
 
 func main() {
 	data := cryptowat.AssetsContainer{}
 	data.SetAsset("kraken", "btcusd")
-	cw := cryptowat.New(&http.Client{}, &data)
+	data.SetAsset("bitfinex", "ltcbtc")
+
+	cw := cryptowat.New(&http.Client{}, &data, cryptowat.RetryPolicy{})
+	ctx := cryptowat.Context{
+		Context: context.Background(),
+	}
 	params := cryptowat.OHLCParams{
 		Periods: []string{"60", "180"},
 	}
-	resp, err := cw.GetAllOHLC(params)
+	ts := time.Now()
+	resp, err := cw.GetAllOHLC(ctx, params)
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
-
+	fmt.Println(time.Since(ts))
 	fmt.Println(resp)
 }
